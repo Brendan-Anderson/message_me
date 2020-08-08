f3d46d9fe0b2097b1da89a07b97a47c4b298938a
Brendan-Anderson <Brendan-Anderson@users.noreply.github.com>
2020-08-07 17:44:49 -0700

    Created User table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Brendan", password: "password")
+User.create(username: "Naruto", password: "password")
+User.create(username: "Sasuke", password: "password")
+User.create(username: "Sakura", password: "password")
+User.create(username: "Kakashi", password: "password")
