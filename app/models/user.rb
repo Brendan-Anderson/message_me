291033ad12540b3787064598755f1715456f12b8
Brendan-Anderson <Brendan-Anderson@users.noreply.github.com>
2020-08-18 19:22:08 -0700

    Add signup page with functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true, length: { minimum: 3, maximum: 15 },
             uniqueness: { case_sensitive: false }
   has_many :messages
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_004745) do
+ActiveRecord::Schema.define(version: 2020_08_19_014146) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_004745) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
